21daa517d41144d01043cc4b76f1f13a3089cc20
MS <3050119305@qq.com>
2018-10-16 16:44:15 +0800

    init

---
+++ test10/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test10
+//
+//  Created by apple on 2018/10/16.
+//  Copyright © 2018年 ms. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
